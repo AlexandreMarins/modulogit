10d4c81ac74fc129ad754b255671aa8f376ee29b
AlexandreMarins <prosperdesigngrafica@gmail.com>
2018-04-25 16:02:38 -0300

    Primeiro Commit

---
+++ ficc/index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>FICC - Festival Internacional de Cinema Cristão</title>
+        <meta name="description" content="Esta página tem o propósito de divulgar o maior festival de cinema cristão da América Latina"/>
+        <meta name="viewport" content="width=decive-width, initia-scale=1.0"/>
+
+        <link rel="shortcut icon" href="_imagens/_logos/_favicon_ficc.png"/>
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+
+    </head>
+
+    <!--INÍCIO DA PÁGINA-->
+    <body>
+        <h1 class="fontzero">Jesus Cristo é o Senhor</h1>
+
+        <section class="barra" title="JESUS CRISTO É O SENHOR!" alt="JESUS CRISTO É O SENHOR!">
+            <h2 class="fontzero">Conheça o Festival Internacional de Cinema Cristão - FICC</h2>
+
+        </section>
+
+        <header class="main_header container">
+
+            <div class="content">
+
+                <img class="main_logo" title="FICC_logo" alt="Logotipo do FICC - Festival de Cinema Cristão" src="_imagens/_logos/logo-headerokfinal.png"/>
+
+                <ul class="main_menu">
+                    <li><a href="regulamento" title="Regulamento do Festival">REGULAMENTO</a></li>
+                    <li><a href="filmes_selecionados" title="Filmes Selecionados">FILMES SELECIONADOS</a></li>
+                    <li><a href="festivais" title="Vejas as edições anteriores do FICC">FESTIVAIS</a></li>
+                    <li><a href="projetos" title="Conheça aqui alguns dos projetos do FICC">PROJETOS</a></li>
+                    <li><a href="investidores" title="Seja um parceiro do FICC nessa missão!">INVESTIDORES</a></li>
+                    <li><a href="credenciamento" title="Para se credenciar">CREDENCIAMENTO</a></li>
+                    <li><a href="contatos" title="Estamos esperando seu contato! - Tire suas dúvidas sobre o FICC">CONTATOS</a></li>             
+                </ul>
+                <div class="clear"></div>
+            </div>
+        </header>
+        <!--FECHA CABEÇALHO-->
+
+        <section class="container banner">
+            <h1 class="fontzero">FICC - Festival Internacional de Cinema Cristão</h1>
+            <div class="fundobanner">
+                <div class="content">                   
+                    <img title="Título da galeria" alt="descrição" src="_imagens/_banners/slider-V-FESTIVAL-2017-h550-INSCRICAO-APRESENTADORES-4.jpg"/>   
+                </div>
+            </div>
+            <div class="clear"></div>
+
+        </section>
+
+        <!--FIM DA GALERIA-->
+
+        <section class="container">
+            <div class="content">
+                <h1 class="fontzero">Conheça o regulamento da inscrição</h1>
+                <div class="fundobotton">
+                    <button class="button" onclick="Votar" accesskey="">Regulamento</button>
+                    <button class="button" onclick="Votar" accesskey="">Inscrição</button>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </section>  
+
+        <!--FIM BUTTON ÁREA-->
+
+
+        <!--RANKING VOTAÇÃO-->
+        <main class="container">
+            <section class="votos container">
+                <h3 class="big_title content">Acompanhe aqui a lista dos 10 mais votados na categoria de Melhor Filme</h3>
+                <div class="content">
+
+                </div>
+
+                <div class="clear"></div>
+
+            </section>
+
+            <!--EDITION ÁREA-->
+            <article>
+                <header>
+                    <h3 class="big_title content">Festival Internacional de Cinema Cristão - FICC</h3>
+                </header>
+                <div class="edition">
+                    <div class="content">
+                        <div>
+                            <iframe width="500" height="300" src="https://www.youtube.com/embed/piCglNAkd-U" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                        </div>
+
+                        <div>
+                            <p class="taglaine">O Festival Internacional de Cinema Cristão está na sua quinta edição. Começou em 2013 com transmissão Ao Vivo direto dos estúdios da TV Boas Novas. E também pela Internet com alcance mundial.
+
+                                Até então era um evento cultural apenas Nacional. Mas chamou a atenção de outros países, que logo manifestaram interesse em participar da mostra e concorrer.
+
+                                Com a inclusão dos filmes estrangeiros na terceira edição e mudança de sigla na quarta, o FICC conquistou maior amplitude e importância no audiovisual.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                </div>
+                <div class="clear"></div>
+
+            </article>
+
+
+            <!--FICC NEWS-->
+            <section class="news container">
+                <h3 class="big_title content">FICC NEWS</h3>
+                <div class="content">
+
+                </div>
+
+                <div class="clear"></div>
+
+            </section>
+
+            <!--FICC VIDEOS-->
+            <section class="videos container">
+                <h3 class="big_title content">Vídeos FICC</h3>
+                <div class="content">
+
+                </div>
+
+                <div class="clear"></div>
+
+            </section>
+
+            <!--ÚLTIMAS POSTAGENS-->
+            <section class="posts container">
+                <h3 class="big_title content">Veja as últimas postagens</h3>
+                <div class="content">
+
+                </div>
+
+                <div class="clear"></div>
+
+            </section>
+
+            <!--EDIÇOES ANTERIORES-->
+            <section class="records container">
+                <h3 class="big_title content">Edições Anteriores do FICC</h3>
+                <div class="content">
+
+                </div>
+
+                <div class="clear"></div>
+
+            </section>
+
+            <!--PARCEIROS ÁREA-->
+            <section class="parceiros container">
+                <h3 class="big_title content">Apoiadores</h3>
+                <div class="content">
+
+                </div>
+
+                <div class="clear"></div>
+
+            </section>
+
+        </main>
+
+        <!--FECHA CONTEÚDO-->
+
+        <footer class="container">
+            <div class="fundorodape container">
+                <div class="content">
+                    <nav>
+                        <h1 class="fontzero">Mapa do site</h1>
+                        <ul>
+                            <li><a href="regulamento" title="Regulamento do Festival">REGULAMENTO</a></li>
+                            <li><a href="filmes_selecionados" title="Filmes Selecionados">FILMES SELECIONADOS</a></li>
+                            <li><a href="festivais" title="Vejas as edições anteriores do FICC">FESTIVAIS</a></li>
+                            <li><a href="projetos" title="Conheça aqui alguns dos projetos do FICC">PROJETOS</a></li>
+                            <li><a href="investidores" title="Seja um parceiro do FICC nessa missão!">INVESTIDORES</a></li>
+                            <li><a href="credenciamento" title="Para se credenciar">CREDENCIAMENTO</a></li>
+                            <li><a href="contatos" title="Estamos esperando seu contato! - Tire suas dúvidas sobre o FICC">CONTATOS</a></li>               
+                        </ul>
+                    </nav>
+                    
+                    <div class="fb-like" data-href="https://www.facebook.com/FestivaldeCinemaFICC/?ref=br_rs" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+
+                </div>
+            </div>
+
+        </footer>
+        <!--FECHA RODAPÉ-->
+
+        <div id="fb-root"></div>
+        <script>
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id))
+                    return;
+                js = d.createElement(s);
+                js.id = id;
+                js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.11';
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));
+        </script>
+
+    </body>
+</html>
---
+++ prosper_design/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title>Prosper Design</title>
+        <meta name="descripition" content="Prosper Design - Design Gráfico - Produção Gráfica - Web Design"/>
+        <meta name="viewport" content="width=device-width, intial-scale=1.0"/> 
+        <!--FAVICON
+            STYLE
+            FONT-->
+
+    </head>
+
+    <body>
+
+        <header>
+            <img title="Prosper Design" alt="[Prosper Design]" src="img/logo_prosper_d_b.png"/>
+            <h1>Prosper Design</h1>
+
+            <ul>
+                <li><a href="home" title="Prosper Design - Design Gráfico - Produção Gráfica - Web Design">Início</a></li>
+                <li><a href="sobre" title="Mais iformações neste menu">Sobre</a></li>
+                <ul>
+                    <li><a href="#prosperdesign" title="Tudo o que você precisa saber sobre nossa empresa especializada em design">Quem Somos</a></li>
+                    <li><a href="servicos" title="Aqui você irá conhecer todos os nossos serviços de design">O que Fazemos</a></li>
+                    <li><a href="parceiros" title="Nesta sessão você irá conhecer as empresas que são os maiores clientes e parceiros da Prosper Design">Nossos Parceiros</a></li>
+                    <li><a href="contato" title="Fale conosco! Aqui você poderá tirar suas dúvidas e pedir orçamentos">Fale com a Gente</a></li>                        
+                </ul>
+                <li><a href="designgrafico" title="Produção de Arte, Publicidade, Programação Visual e Design Gráfico">Design Gráfico</a></li>
+                <li><a href="producaografica" title="Materializamos suas idéias e conceitos criados através de layouts utilizando as técnicas mais avançadas em pré-impressão e impressão.">Produção Gráfica</a></li>
+                <li><a href="webdesign" title="Criamos sites e páginas WEB que vendem!">Web Design</a></li>
+                <li><a href="portifolio" title="Conheça alguns de nossos projetos de sucesso!">Projetos</a></li>            
+            </ul>
+            <!--INÍCIO GALERIA-->
+            <section>
+                <header>
+                    <h1>Prosper Design</h1>
+                </header>    
+                    <div class="galeria">
+                        
+                        
+                    </div>                
+            </section>
+            <!--FECHA GALERIA-->               
+        </header>            
+        <!--FECHA CABEÇALHO-->   
+        
+        <main>
+            <!--DESTAQUE DO SITE-->
+            <section>
+                <header>
+                    <h1>Temos a solução que você precisa para a realização do seu projeto.</h1>
+                </header>
+
+                <article>
+                    <p class="tagline">Desenvolvimento de projetos digitais e impressos.
+                    <img title="" alt="" src=""/>    
+
+
+                </article> 
+                <!--FECHA DESTAQUE DO SITE-->
+            </section>
+        </main>
+        <!--FECHA CONTEUDO DA PAGINA-->
+
+        <footer>
+            <h1>Prosper Design</h1>
+            <nav>
+                <h1>Mapa do Site</h1>
+                <ul>
+                    <li><a href="home" title="Prosper Design - Design Gráfico - Produção Gráfica - Web Design">Início</a></li>
+                    <li><a href="designgrafico" title="Produção de Arte, Publicidade, Programação Visual e Design Gráfico">Design Gráfico</a></li>
+                    <li><a href="producaografica" title="Materializamos suas idéias e conceitos criados através de layouts utilizando as técnicas mais avançadas em pré-impressão e impressão.">Produção Gráfica</a></li>
+                    <li><a href="webdesign" title="Criamos sites e páginas WEB que vendem!">Web Design</a></li>
+                    <li><a href="portifolio" title="Conheça alguns de nossos projetos de sucesso!">Projetos</a></li>            
+                </ul> 
+
+                <div>
+                    <ul>
+                        <li><a href="facebook" title="Prosper Design no Facebook">Facebook</a></li>
+                        <li><a href="instagram" title="Prosper Design no Instagram">Instagram</a></li>
+                        <li><a href="tweeter" title="Prosper Design no Tweeter">Tweeter</a></li>
+
+
+                    </ul>
+
+                </div>
+            </nav>
+        </footer>
+        <!--FECHA RODAPÉ-->
+
+    </body>
+
+</html>
